6a8bb8bef1c28e413abaa23945bcd02a43234fb9
juan.paez <juan.paez@dms.ms>
2024-09-07 23:50:00 -0500

    FetchData

---
+++ nextproject/app/posts/page.jsx
@@ -0,0 +1,17 @@
+async function loadPosts() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await res.json();
+    console.log(data)
+}
+
+
+function PostPages() {
+
+    loadPosts();
+
+    return (
+        <div>PostPages</div>
+    )
+}
+
+export default PostPages
