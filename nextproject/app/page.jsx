79c78fac451906e8f6521b9f0a51f14abf34b76d
juansubas <juansubas@gmail.com>
2024-09-07 22:04:11 -0500

    Add

--- nextproject/app/page.jsx
+++ nextproject/app/page.jsx
@@ -1,3 +1,14 @@
-export default function Home() {
-    return <h1>Home</h1>
+import Users from "@/components/Users"
+
+export const metadata = {
+    title: 'Mi pagina especial Home'
+}
+
+export default function HomePage() {
+    //Server component
+    return <section>
+        {/* Client Component */}
+        <h1>Hello World</h1>
+        <Users/>
+    </section>
 }
