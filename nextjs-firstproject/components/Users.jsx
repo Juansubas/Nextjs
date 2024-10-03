f93aa1230bd11dacccd5969f2f8ff7b3a9189cb8
juansubas <juansubas@gmail.com>
2024-10-02 19:09:30 -0500

    Example

---
+++ nextjs-firstproject/components/Users.jsx
@@ -0,0 +1,24 @@
+"use client";
+
+function Users({users}) {
+  return (
+    <ul>
+      {users.map((user) => (
+        <li
+          key={user.id}
+          className="bg-slate-400 mb-2 p-4 rounded-md text-black flex justify-between"
+        >
+          <div>
+            <h5 className="font-bold">
+              {user.id} {user.first_name} {user.last_name}
+            </h5>
+            <p className="text-slate-100">email: {user.email}</p>
+          </div>
+          <img src={user.avatar} alt="" className="rounded-full w-16" />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default Users;
