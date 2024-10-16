c14373fb4ecbfa0355fcd352a3748ad9609a9dbe
juan.paez <juan.paez@dms.ms>
2024-10-16 14:27:51 -0500

    Update

--- nextproject/app/about/page.jsx
+++ nextproject/app/about/page.jsx
@@ -3,5 +3,17 @@ export const metadata = {
 }
 
 export default function AboutPage() {
-    return <h1>About</h1>
+    return (
+        <section>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit minus pariatur reiciendis, magni, id nobis accusamus rem sapiente architecto quos incidunt voluptatem minima illo nostrum! Modi rem maxime dignissimos harum.</p>
+            <button className="bg-sky-400 px-3 py-2 rounded-md" onClick={() => {
+
+                }
+            }
+            >
+                Click
+            </button>
+        </section>
+    )
 }
