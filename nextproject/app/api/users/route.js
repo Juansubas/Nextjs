df4e4e2dc6dee12be4728a56973aa15885537337
juansubas <juansubas@gmail.com>
2024-10-24 23:57:01 -0500

    Finish Example

--- nextproject/app/api/users/route.js
+++ nextproject/app/api/users/route.js
@@ -1,3 +1,30 @@
-export function GET() {
-    return new Response("Hello world");
+import { NextResponse } from 'next/server'
+
+console.log(process.env.TOKEN);
+
+export async function GET() {
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    return NextResponse.json(data);
+}
+
+export async function POST(request) {
+    const data = await request.json(); // Este es el request body tradicional
+    return NextResponse.json({
+        message: 'Creando datos!'
+    });
+}
+
+export function PUT() {
+    return NextResponse.json({
+        message: 'Actualizando datos!'
+    });
+}
+
+export function DELETE() {
+    return NextResponse.json({
+        message: 'Eliminando datos!'
+    });
 }
