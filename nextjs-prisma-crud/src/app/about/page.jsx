44f5503e7e2615374c4a3be1a424b3c56318b109
juansubas <juansubas@gmail.com>
2024-10-28 15:34:00 -0500

    About Page

--- nextjs-prisma-crud/src/app/about/page.jsx
+++ nextjs-prisma-crud/src/app/about/page.jsx
@@ -1,7 +1,22 @@
 function AboutPage() {
   return (
-    <div>AboutPage</div>
-  )
+    <section className="container mx-auto">
+      <h3 className="text-4xl font-bold my-5">About</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat,
+        quaerat! Aliquam, nisi nostrum? Provident voluptate earum voluptatum
+        consequatur iste cupiditate, rerum suscipit possimus quasi debitis totam
+        nesciunt voluptatem porro sit eaque eligendi exercitationem. Aspernatur
+        dicta sed iusto aperiam error modi in laudantium, maiores rerum fugit
+        suscipit ex, velit autem magni impedit praesentium aut ullam tenetur
+        dignissimos? Odit atque, quia assumenda laborum error exercitationem
+        quasi, quo rem quis vel sapiente culpa soluta at unde ab quibusdam minus
+        laudantium labore, minima cum. Laborum vel ut, mollitia nihil veritatis
+        vitae voluptates aliquid, perspiciatis cum commodi velit doloribus
+        asperiores repellat fugiat fuga natus minima.
+      </p>
+    </section>
+  );
 }
 
-export default AboutPage
+export default AboutPage;
